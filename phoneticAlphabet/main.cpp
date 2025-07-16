3044f711f851d2a35dd6acf46b100841e394f6d0
isuckatgamedev <jiorgose@gmail.com>
2025-07-16 11:38:36 +0200

    implemented alphabet

--- phoneticAlphabet/main.cpp
+++ phoneticAlphabet/main.cpp
@@ -1,5 +1,25 @@
 #include <iostream>
+#include <string>
+#include <map>
+#include <cstdlib>
+#include <ctime>
+
+std::map<char, std::string> phoneticAlphabet = {
+  {'a', "alpha"},   {'b', "bravo"},    {'c', "charlie"},
+  {'d', "delta"},   {'e', "echo"},     {'f', "foxtrot"},
+  {'g', "golf"},    {'h', "hotel"},    {'i', "india"},
+  {'j', "juliett"}, {'k', "kilo"},     {'l', "lima"},
+  {'m', "mike"},    {'n', "november"}, {'o', "oscar"},
+  {'p', "papa"},    {'q', "quebec"},   {'r', "romeo"},
+  {'s', "sierra"},  {'t', "tango"},    {'u', "uniform"},
+  {'v', "victor"},  {'w', "whiskey"},  {'x', "x-ray"},
+  {'y', "yankee"},  {'z', "zulu"}
+};
 
 int main() {
-  std::cout << "test" << std::endl;
+  for (const auto& pair : phoneticAlphabet) {
+    std::cout << pair.first << ": " << pair.second << std::endl;
+  }
+
+  std::cin.get();
 }
